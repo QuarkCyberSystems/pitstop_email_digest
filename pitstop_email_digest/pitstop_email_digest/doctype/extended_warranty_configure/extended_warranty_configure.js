cf28f2404a3eedd9a03440456e168f238e84f14b
kunhimohamed <kunhimohamed6@gmail.com>
2026-06-23 20:56:59 +0400

    fix(Extended Warranty): at config default can check multiple if different item code

--- pitstop_email_digest/pitstop_email_digest/doctype/extended_warranty_configure/extended_warranty_configure.js
+++ pitstop_email_digest/pitstop_email_digest/doctype/extended_warranty_configure/extended_warranty_configure.js
@@ -1,22 +1,8 @@
 // Copyright (c) 2026, QCS and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Extended Warranty Configure Details", {
-	default(frm, cdt, cdn) {
-		console.log("here we go");
-		const row = locals[cdt][cdn];
+// frappe.ui.form.on("Extended Warranty Configure Details", {
+// 	refresh(frm) {
 
-		if (row.default) {
-			frm.doc.extended_warranty_configure_details.forEach((d) => {
-				if (d.name !== row.name) {
-					if (d.default) {
-						row.default = 0;
-						frappe.msgprint("Only one raw can be default check box");
-					}
-				}
-			});
-
-			frm.refresh_field("extended_warranty_configure_details");
-		}
-	},
-});
+// 	}
+// });
--- pitstop_email_digest/pitstop_email_digest/doctype/extended_warranty_configure/extended_warranty_configure.py
+++ pitstop_email_digest/pitstop_email_digest/doctype/extended_warranty_configure/extended_warranty_configure.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2026, QCS and contributors
 # For license information, please see license.txt
 
+from collections import defaultdict
+
 import frappe
 from frappe.model.document import Document
 
@@ -12,11 +14,19 @@ class ExtendedWarrantyConfigure(Document):
         self.check_single_default()
 
     def check_single_default(self):
-        default_rows = [
-            d for d in self.extended_warranty_configure_details if d.default
-        ]
-        if len(default_rows) > 1:
-            frappe.throw("Only one row can be marked as Default")
+        item_defaults = defaultdict(list)
+
+        for d in self.extended_warranty_configure_details:
+            if d.default:
+                item_defaults[d.warranty_item].append(d.idx)
+
+        for item_code, rows in item_defaults.items():
+            if len(rows) > 1:
+                frappe.throw(
+                    f"Item <b>{item_code}</b> has multiple rows marked as Default "
+                    f"(rows: {', '.join(map(str, rows))}). "
+                    f"Only one row per item can be marked as Default."
+                )
 
     def check_duplicate_item_supplier(self):
         supplier_item_code_set = set()
