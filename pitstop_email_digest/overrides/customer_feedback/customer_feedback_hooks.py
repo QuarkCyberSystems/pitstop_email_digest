6fd9dbdce5992191ae9e8164f49761f5344bbfe9
kunhimohamed <kunhimohamed6@gmail.com>
2026-07-09 13:08:56 +0400

    fix(Callcenter API): abstract the webhook using methods

--- pitstop_email_digest/overrides/customer_feedback/customer_feedback_hooks.py
+++ pitstop_email_digest/overrides/customer_feedback/customer_feedback_hooks.py
@@ -1,5 +1,3 @@
-import json
-
 import frappe
 
 
@@ -9,60 +7,7 @@ def after_insert(doc, method=None):
     if not settings.enable:
         return
 
-    campaign = frappe.get_all(
-        "Campaign Details",
-        filters={
-            "parent": "Genesis Settings",
-            "parentfield": "campaign_details",
-            "campaign_doctype": doc.doctype,
-        },
-        fields=["campaign_url", "field_map", "campaign_name"],
-        limit_page_length=1,
-    )
+    campaign = settings.get_campaign_details(doc)
 
     if campaign:
-        campaign_url = campaign[0].campaign_url
-        field_map = campaign[0].field_map
-        campaign_name = campaign[0].campaign_name
-        field_map = json.loads(field_map) if field_map else {}
-        if campaign_url:
-            payload = build_cfb_payload(doc, field_map)
-            frappe.enqueue_doc(
-                "Genesis Settings",
-                "Genesis Settings",
-                "send_to_genesis",
-                queue="short",
-                url=campaign_url,
-                payload=payload,
-                reference_doctype=doc.doctype,
-                reference_name=doc.name,
-                campaign_name=campaign_name,
-            )
-            # settings.send_to_genesis(
-            #     url=campaign_url,
-            #     payload=payload,
-            #     reference_doctype=doc.doctype,
-            #     reference_name=doc.name,
-            #     campaign_name=campaign_name,
-            # )
-    return campaign
-
-
-def build_cfb_payload(doc, field_map=None):
-    field_map = field_map or {}
-    doc_dict = doc.as_dict()
-    updated_dict = {}
-    free_counter = 0
-
-    for each_key, mapped_key in field_map.items():
-        updated_dict[mapped_key] = doc_dict.get(each_key) or "N/A"
-
-    for each_key, value in doc_dict.items():
-        if each_key not in field_map:
-            free_counter += 1
-            updated_dict[f"free{free_counter}"] = value or "N/A"
-            # Contact lists do not support more than 10 extra data columns.
-            if free_counter >= 10:
-                break
-
-    return updated_dict
+        settings.send_campaign(campaign, doc)
--- pitstop_email_digest/pitstop_email_digest/doctype/genesis_settings/genesis_settings.py
+++ pitstop_email_digest/pitstop_email_digest/doctype/genesis_settings/genesis_settings.py
@@ -115,3 +115,62 @@ class GenesisSettings(Document):
                 campaign_url=url,
                 campaign_name=campaign_name,
             )
+
+    def get_campaign_details(self, doc):
+        campaign = frappe.get_all(
+            "Campaign Details",
+            filters={
+                "parent": "Genesis Settings",
+                "parentfield": "campaign_details",
+                "campaign_doctype": doc.doctype,
+            },
+            fields=["campaign_url", "field_map", "campaign_name"],
+            limit_page_length=1,
+        )
+        return campaign
+
+    def build_payload(self, doc, field_map=None):
+        field_map = field_map or {}
+        doc_dict = doc.as_dict()
+        updated_dict = {}
+        free_counter = 0
+
+        for each_key, mapped_key in field_map.items():
+            updated_dict[mapped_key] = doc_dict.get(each_key) or "N/A"
+
+        for each_key, value in doc_dict.items():
+            if each_key not in field_map:
+                free_counter += 1
+                updated_dict[f"free{free_counter}"] = value or "N/A"
+                # Contact lists do not support more than 10 extra data columns.
+                if free_counter >= 10:
+                    break
+
+        return updated_dict
+
+    def send_campaign(self, campaign, doc):
+        campaign_url = campaign[0].campaign_url
+        field_map = campaign[0].field_map
+        campaign_name = campaign[0].campaign_name
+        field_map = json.loads(field_map) if field_map else {}
+        if campaign_url:
+            payload = self.build_payload(doc, field_map)
+            frappe.enqueue_doc(
+                "Genesis Settings",
+                "Genesis Settings",
+                "send_to_genesis",
+                queue="short",
+                url=campaign_url,
+                payload=payload,
+                reference_doctype=doc.doctype,
+                reference_name=doc.name,
+                campaign_name=campaign_name,
+            )
+            # sometime the mac and ssl worker and bench ssl are not same
+            # self.send_to_genesis(
+            # 	url=campaign_url,
+            # 	payload=payload,
+            # 	reference_doctype=doc.doctype,
+            # 	reference_name=doc.name,
+            # 	campaign_name=campaign_name,
+            # )
