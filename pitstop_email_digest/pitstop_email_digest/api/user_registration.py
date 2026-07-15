36b4688079a0e54019f992a7aaf7bfb02a489551
kunhimohamed <kunhimohamed6@gmail.com>
2026-07-15 12:00:36 +0400

    fix[Portal/User Registration]: create user if customer is existing

--- pitstop_email_digest/pitstop_email_digest/api/user_registration.py
+++ pitstop_email_digest/pitstop_email_digest/api/user_registration.py
@@ -2,20 +2,27 @@ import frappe
 from frappe import _
 from frappe.permissions import add_user_permission
 from frappe.utils import cint, escape_html
-from frappe.utils.nestedset import get_root_of
 
 
 @frappe.whitelist(allow_guest=True)
-def sign_up(email: str, full_name: str, password: str, phone: str) -> tuple[int, str]:
-    user = frappe.db.get("User", {"email": email})
-    if user:
-        result = register_customer(
-            customer_name=full_name, email=email, mobile_no=phone, password=password
-        )
-        if result and result.get("customer_id"):
+def sign_up(
+    email: str, full_name: str, password: str, mobile_no: str
+) -> tuple[int, str]:
+    result = check_customer(email=email, mobile_no=mobile_no, password=password)
+    if result and result.get("customer_id"):
+        user_created = create_user(full_name, email, mobile_no, password)
+        if user_created:
             add_user_permission(
                 "Customer", result["customer_id"], email, ignore_permissions=True
             )
+            return {"success": True, "message": "User created successfully"}
+    else:
+        return {"success": False, "message": _("Customer Not Exists")}
+
+
+def create_user(full_name, email, mobile_no, password, **kwargs):
+    user = frappe.db.get("User", {"email": email})
+    if user:
         if user.enabled:
             return {"success": False, "message": _("Already Registered")}
         else:
@@ -39,34 +46,24 @@ def sign_up(email: str, full_name: str, password: str, phone: str) -> tuple[int,
                 "doctype": "User",
                 "email": email,
                 "first_name": escape_html(full_name),
+                "phone": mobile_no,
                 "enabled": 1,
                 "new_password": password,
-                "user_type": "System User",
+                "user_type": "Website User",
             }
         )
         user.flags.ignore_permissions = True
         user.flags.ignore_password_policy = True
         user.insert()
 
-        result = register_customer(
-            customer_name=full_name, email=email, mobile_no=phone, password=password
-        )
-
-        # add user permission so this user can only access their own Customer record
-        if result and result.get("customer_id"):
-            add_user_permission(
-                "Customer", result["customer_id"], email, ignore_permissions=True
-            )
-
         # set default signup role as per Portal Settings
         default_role = frappe.get_single_value("Portal Settings", "default_role")
         if default_role:
             user.add_roles(default_role)
+    return True
 
-    return {"success": True, "message": "User created successfully"}
 
-
-def register_customer(customer_name, email, mobile_no, **kwargs):
+def check_customer(email, mobile_no, **kwargs):
     existing = False
     if email:
         existing = frappe.db.exists("Customer", {"email_id": email})
@@ -81,34 +78,3 @@ def register_customer(customer_name, email, mobile_no, **kwargs):
             "email_id": email,
             "phone": mobile_no,
         }
-
-    customer = frappe.get_doc(
-        {
-            "doctype": "Customer",
-            "customer_name": customer_name,
-            "customer_type": "Individual",
-            "customer_group": "Individual",
-            "territory": get_root_of("Territory"),
-        }
-    )
-    customer.insert(ignore_permissions=True)
-
-    # Optionally create linked Contact
-    contact = frappe.get_doc(
-        {
-            "doctype": "Contact",
-            "first_name": customer_name,
-            "email_ids": [{"email_id": email, "is_primary": 1}],
-            "phone_nos": [{"phone": mobile_no, "is_primary_mobile_no": 1}],
-        }
-    )
-    contact.insert(ignore_permissions=True)
-    contact.append("links", {"link_doctype": "Customer", "link_name": customer.name})
-    contact.save(ignore_permissions=True)
-
-    return {
-        "customer_id": customer.name,
-        "customer": customer.customer_name,
-        "email_id": email,
-        "phone": mobile_no,
-    }
