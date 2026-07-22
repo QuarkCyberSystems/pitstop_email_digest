f745e4477b1bca42962ee4362f8b80974f8bdcd8
kunhimohamed <kunhimohamed6@gmail.com>
2026-07-22 12:18:21 +0400

    fix: updated vendor send data method

--- pitstop_email_digest/utils/send_data_vendor/send_data_genesys.py
+++ pitstop_email_digest/utils/send_data_vendor/send_data_genesys.py
@@ -6,7 +6,7 @@ import frappe
 def send_data_genesys(doc, campaing_name=None, extra_key_args=None):
     settings = frappe.get_single("Genesys Settings")
 
-    if not settings.enable:
+    if not settings.enable or frappe.conf.developer_mode:
         return
 
     campaign = settings.get_campaign_details(doc, campaing_name)
